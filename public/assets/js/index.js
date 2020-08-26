01624e2417a1ac3c0b75506bbd96e3a967da656c
Santiago Solana <santiago.sjs@gmail.com>
2020-08-26 12:57:16 -0400

    Added working HTML routes/ Started API routes/ Encountering some issues with api routes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,11 @@
+const allNotes = require("./../../../server");
+
 const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { Console } = require("console");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,6 +9,34 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(__dirname + "/public"));
+
+// HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// API Routes
+const allNotes = [];
+
+app.get("/api/notes", (req, res) => {
+  res.json("/db/db.json");
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  allNotes.push(newNote);
+
+  res.json(newNote);
+  console.log("saved notes: " + JSON.stringify(allNotes))
+});
+
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
+
+module.exports = allNotes;
